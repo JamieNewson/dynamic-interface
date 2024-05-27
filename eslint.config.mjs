cfe2767c76ebba7ff9a371671b7420c9880ee4eb
Jamie Newson <79523465+JamieNewson@users.noreply.github.com>
2024-05-27 18:12:43 +0100

    ESLint Config Updated for Node/Webpack

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      env: {
+        browser: true,
+        node: true,
+        es6: true,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
